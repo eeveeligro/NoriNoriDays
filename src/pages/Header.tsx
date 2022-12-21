903e7fd00067d55c66a4f263d994342f43b10dc0
a-orgil <aorgil@otas-kk.co.jp>
2022-12-21 17:59:00 +0900

    20221221

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -11,14 +11,14 @@ function Header() {
     <AppBar position="static" sx = {{bgcolor:"primary"}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <FactoryIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <FactoryIcon sx={{ mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
             component="a"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              // display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
@@ -26,27 +26,7 @@ function Header() {
               textDecoration: 'none',
             }}
           >
-            検品・照合アプリ
-          </Typography>
-
-          <FactoryIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            // href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            検品・照合アプリ
+            ノリノリデイズ
           </Typography>
         </Toolbar>
       </Container>
